7023c415a519239f3e8396b994d0251cb621c828
Russell Webster <russell@jossell.com>
2022-05-07 17:42:10 +0100

    Add UI/front-end for marking an item as completed

--- web/src/app/components/todo-list/todo-list.component.ts
+++ web/src/app/components/todo-list/todo-list.component.ts
@@ -3,7 +3,8 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { BehaviorSubject, catchError, map, Observable, throwError, tap } from 'rxjs';
 import { TodoService } from 'src/app/core/services/todo.service';
 import { BaseComponent } from '../base.component';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { TodoItem } from 'src/app/core/models';
 
 @Component({
   selector: 'app-todo-list',
@@ -12,11 +13,12 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
 })
 export class TodoListComponent extends BaseComponent {
 
-  busySubject = new BehaviorSubject<boolean>(false);
-  busy: boolean = false;
+  formBusySubject = new BehaviorSubject<boolean>(false);
+  formBusy: boolean = false;
   vm$: Observable<any>;
   form: FormGroup;
   faCheck = faCheck;
+  faXmark = faXmark;
 
   constructor(
     private todoService: TodoService,
@@ -39,23 +41,40 @@ export class TodoListComponent extends BaseComponent {
       })
     );
 
-    this.busySubject.pipe(
+    this.formBusySubject.pipe(
       tap(_ => {
-        this.busy = _;
+        this.formBusy = _;
       })
     ).subscribe();
   }
 
   submit() {
-    this.busySubject.next(true);
+    this.formBusySubject.next(true);
     this.todoService.create(this.form.get('text')?.value)
       .subscribe({
         complete: () => {
-          this.busySubject.next(false);
+          this.formBusySubject.next(false);
           this.form.reset();
         },
         error: err => {
           this.handleError('Failed to create new item', err.message);
+          return throwError(() => err);
+        }
+      });
+  }
+
+  setCompleted(item: TodoItem) {
+    this.todoService.setCompleted(item.id, new Date())
+      .subscribe({
+        complete: () => {
+          console.log("Completed call");
+        },
+        next: _ => {
+          console.log(_);
+          item = _;
+        },
+        error: err => {
+          this.handleError('Failed to update existing item', err.message);
           return throwError(() => err);
         }
       });
